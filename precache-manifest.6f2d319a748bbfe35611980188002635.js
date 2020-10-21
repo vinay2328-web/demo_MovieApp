45b9ca58c577aa389284bf863920f0d44ed77aa6
= vinay <= vinaypathrikar2328@gmail.com>
2020-10-21 19:34:34 +0530

    Updates

---
+++ precache-manifest.6f2d319a748bbfe35611980188002635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1455946957ea259be691071d82473990",
+    "url": "/demo_MovieApp/index.html"
+  },
+  {
+    "revision": "62f6b52f9e8fee72bdad",
+    "url": "/demo_MovieApp/static/css/main.0e07b7be.chunk.css"
+  },
+  {
+    "revision": "623778d38451ced31225",
+    "url": "/demo_MovieApp/static/js/2.4143be4f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo_MovieApp/static/js/2.4143be4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f6b52f9e8fee72bdad",
+    "url": "/demo_MovieApp/static/js/main.b0e0561e.chunk.js"
+  },
+  {
+    "revision": "6577d033598bd7d9b171",
+    "url": "/demo_MovieApp/static/js/runtime-main.31ce1969.js"
+  }
+]);
